0d10898dfb1f1e65b8b4bfe5508988423358c382
Nidhalcherif <cherif.nidhal@gmail.com>
2023-04-10 11:04:22 +0100

    crud Livre sans template

---
+++ src/Controller/LivresController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Livres;
+use App\Repository\LivresRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LivresController extends AbstractController
+{
+    #[Route('/admin/livres', name: 'app_livres')]
+    public function index(LivresRepository $rep): Response
+    { $livres= $rep->findAll();
+       dd($livres);
+    }
+    #[Route('/admin/livres/find/{id}', name: 'app_livres_find_id')]
+    public function chercher(Livres $livre): Response
+    {   dd($livre);
+    }
+
+    #[Route('/admin/livres/add', name: 'app_livres_add')]
+    public function add(ManagerRegistry $doctrine): Response
+    { $livre=new Livres();
+        $date= new \DateTime('2022-01-01');
+        $livre->setLibelle('Réseaux locaux')
+              ->setImage('https://via.placeholder.com/300')
+              ->setPrix(200)
+              ->setEditeur('DUNOD')
+              ->setDateEdition($date)
+             ->setResume('text...............');
+        $em=$doctrine->getManager();
+        $em->persist($livre);
+        $em->flush();
+        dd($livre);
+
+    }
+    #[Route('/admin/livres/update/{id}', name: 'app_livres_find_id')]
+    public function update_price(Livres $livre,ManagerRegistry $doctrine): Response
+    {
+
+        $livre->setPrix(110);
+        $em=$doctrine->getManager();
+        $em->flush();
+        dd($livre);
+    }
+    #[Route('/admin/livres/delete/{id}', name: 'app_livres_find_id')]
+    public function delete(Livres $livre,ManagerRegistry $doctrine): Response
+    {
+
+        $em=$doctrine->getManager();
+        $em->remove($livre);
+        $em->flush();
+        dd($livre);
+    }
+
+
+}
