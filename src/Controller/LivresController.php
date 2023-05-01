0af1969923e5eecd1019e08abe26f742ffb55702
Nidhalcherif <cherif.nidhal@gmail.com>
2023-05-01 11:28:56 +0100

    creation d une relation ManyToOne et utilisation de KNpPaginator

--- src/Controller/LivresController.php
+++ src/Controller/LivresController.php
@@ -5,17 +5,24 @@ namespace App\Controller;
 use App\Entity\Livres;
 use App\Repository\LivresRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LivresController extends AbstractController
 {
     #[Route('/admin/livres', name: 'app_livres')]
-    public function findAll(LivresRepository $rep): Response
+    public function findAll(LivresRepository $rep,Request $request,PaginatorInterface $paginator): Response
     { $livres= $rep->findAll();
-       //dd($livres);
-        return $this->render('Livres/findAll.html.twig',['livres'=>$livres]);
+        $pagination = $paginator->paginate(
+            $livres, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+       //dd($livres); //tableau d'objets livre
+        return $this->render('Livres/findAll.html.twig',['livres'=>$pagination]);
     }
     #[Route('/admin/livres/find/{id}', name: 'app_livres_find_id')]
     public function find(Livres $livre): Response
@@ -50,11 +57,12 @@ class LivresController extends AbstractController
     #[Route('/admin/livres/delete/{id}', name: 'app_livres_delete_id')]
     public function delete(Livres $livre,ManagerRegistry $doctrine): Response
     {
-
         $em=$doctrine->getManager();
         $em->remove($livre);
         $em->flush();
-        dd($livre);
+        //Return $this->redirectToRoute('app_livres_update_id',['id'=>4]);
+        //dd($livre);
+        Return $this->redirectToRoute('app_livres');
     }
 
 
--- src/DataFixtures/LivresFixtures.php
+++ src/DataFixtures/LivresFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Categories;
 use App\Entity\Livres;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,17 +12,26 @@ class LivresFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {  $faker = Factory::create('fr_FR');
-        // $faker = Factory::create('ar_SA');
-        for ($i=1;$i<=100;$i++)
-    {     $livre= new Livres();
-          $livre->setLibelle($faker->name())
-            ->setPrix(random_int(10,200))
-            ->setResume($faker->text())
-            ->setEditeur($faker->company())
-            ->setDateEdition(new \DateTime('2022-01-01'))
-            ->setImage('https://picsum.photos/200/?random='.$i);
-        $manager->persist($livre);
+        //$faker = Factory::create('ar_SA');
 
+    for ($j=1;$j<=3;$j++) {
+        $cat = new Categories();
+        $cat->setLibelle($faker->name())
+             ->setDescription($faker->text());
+        $manager->persist($cat);
+        for ($i = 1; $i <= random_int(5, 15); $i++) {
+            $livre = new Livres();
+            $livre->setLibelle($faker->name())
+                ->setPrix(random_int(10, 200))
+                ->setResume($faker->text())
+                ->setEditeur($faker->company())
+                ->setDateEdition(new \DateTime('2022-01-01'))
+                ->setImage('https://picsum.photos/200/?random=' . $i)
+                ->setCategorie($cat);
+
+            $manager->persist($livre);
+
+        }
     }
 
 $manager->flush();
